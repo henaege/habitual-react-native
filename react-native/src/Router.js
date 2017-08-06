9fa6b0b7c05ce52eef441787227301ed7db44487
henaege <henaege@gmail.com>
2017-08-06 16:29:07 -0400

    I don’t wanna commit

--- react-native/src/Router.js
+++ react-native/src/Router.js
@@ -23,7 +23,7 @@ const RouterComponent = () => {
       <Scene    key="main" title="Your Habits:" >
         <Scene key='habitsList' title="Your Habits" component={HabitsList} onRight={()=> Actions.addHabit()} rightTitle="Add" navigationBarStyle={{backgroundColor:"#48A9A6", opacity: 0.8, paddingTop: (Platform.OS === 'android') ? 14 : 0, paddingBottom: (Platform.OS === 'android') ? 24 : 0, borderColor: 'transparent'}} initial/>
 
-        <Scene key="addHabit" component={CategoriesList} title="Add habit" navigationBarStyle={{backgroundColor:(Platform.OS === 'android') ? 'transparent': "#48A9A6", opacity: 0.8, paddingTop: (Platform.OS === 'android') ? 14 : 0, paddingBottom: (Platform.OS === 'android') ? 24 : 0, borderColor: 'transparent'}}/>
+        <Scene key="addHabit" component={AddHabit} title="Add habit" navigationBarStyle={{backgroundColor:(Platform.OS === 'android') ? 'transparent': "#48A9A6", opacity: 0.8, paddingTop: (Platform.OS === 'android') ? 14 : 0, paddingBottom: (Platform.OS === 'android') ? 24 : 0, borderColor: 'transparent'}}/>
       </Scene>
     </Router>
   )
